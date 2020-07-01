2c381240e0e5e7cce8c5dca6550cc311031d7951
Jesvin Thekkekara <jesvinthekkekara.jt@gmail.com>
2020-07-02 00:26:44 +0530

    second commit

--- wk3/redux/src/App.js
+++ wk3/redux/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 
 
@@ -10,11 +13,13 @@ import { BrowserRouter } from 'react-router-dom';
     
     render() {
         return (
+          <Provider store={store}>
           <BrowserRouter>
           <div >
             <Main />
           </div>
         </BrowserRouter>
+        </Provider>
         )
     }
 }
